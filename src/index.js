6142e69fa45fcb53484f8f0bb36cbdffbf9d75ef
Gambit142 <nzefrancismaryeugorji@gmail.com>
2021-09-22 17:42:08 +0100

    Add functions to show the actions of the user when the checkbox is checked

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import './style.css';
+import { interaction } from './interactive.js';
 
 const TODOLIST_CONTAINER = document.querySelector('.todo-lists-div');
 const TODOLIST_ARRAY = [
@@ -24,8 +25,8 @@ const createToDoListDiv = (array) => {
   array.forEach((div) => {
     task += `<div class="task-div">
     <div>
-    <input type="checkbox" id="task-${div.index}" name="task-${div.index}" value="${div.completed}">
-    <label for="task-${div.index}" class="task-description">${div.description}</label><br>
+    <input type="checkbox" data-target="task-${div.index}" id="${div.index}" name="task-${div.index}" value="${div.completed}">
+    <label for="task-${div.index}" id="task-${div.index}" class="task-description">${div.description}</label><br>
     </div>
     <div><i class="fas fa-ellipsis-v"></i></div>
     </div>`;
@@ -44,4 +45,42 @@ const sortingFunction = (a, b) => {
 };
 
 TODOLIST_ARRAY.sort(sortingFunction);
+
+// TODOLIST_ARRAY.forEach((object) => {
+// });
+localStorage.setItem('listOfTasks', JSON.stringify(TODOLIST_ARRAY));
+
 createToDoListDiv(TODOLIST_ARRAY);
+const checkboxes = document.querySelectorAll('input[type="checkbox"]');
+console.log(checkboxes);
+checkboxes.forEach((checkbox) => {
+  const textDescription = document.getElementById(checkbox.dataset.target);
+  console.log(checkbox);
+  checkbox.addEventListener('change', (e) => {
+    console.log('Hello!');
+    // TODOLIST_ARRAY.forEach((item) => {
+    //   if (item.index === checkbox.id) {
+    //     item.completed = e.target.checked;
+    //     // console.log(item);
+    //   }
+    // });
+    // localStorage.setItem('listOfTasks', JSON.stringify(TODOLIST_ARRAY));
+    if (e.target.checked) {
+      textDescription.style.textDecoration = 'line-through';
+    } else {
+      textDescription.style.textDecoration = 'none';
+    }
+    interaction(e);
+  });
+});
+
+function onPageLoad() {
+  if (JSON.parse(localStorage.getItem('listOfTasks'))) {
+    const listTasks = JSON.parse(localStorage.getItem('listOfTasks'));
+    createToDoListDiv(listTasks);
+  }
+}
+
+window.addEventListener('load', () => {
+  onPageLoad();
+});
