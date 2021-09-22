86a4c0b2de629755e50933cc6640be94578b8d27
Gambit142 <nzefrancismaryeugorji@gmail.com>
2021-09-22 21:02:00 +0100

    Create local storage and update it each time there is an event in each checkbox

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import './style.css';
-import { interaction } from './interactive.js';
+import interaction from './interactive.js';
 
 const TODOLIST_CONTAINER = document.querySelector('.todo-lists-div');
-const TODOLIST_ARRAY = [
+let todoListArray = [
   {
     description: 'Do the laundry',
     completed: false,
@@ -20,18 +20,36 @@ const TODOLIST_ARRAY = [
   },
 ];
 
+const CURRENT_STORAGE = JSON.parse(localStorage.getItem('listOfTasks'));
+if (CURRENT_STORAGE) {
+  todoListArray = CURRENT_STORAGE;
+}
+
 const createToDoListDiv = (array) => {
   let task = '';
   array.forEach((div) => {
+    let checked = '';
+    let state = 'none';
+    if (div.completed) {
+      checked = 'checked';
+      state = 'line-through';
+    }
     task += `<div class="task-div">
     <div>
-    <input type="checkbox" data-target="task-${div.index}" id="${div.index}" name="task-${div.index}" value="${div.completed}">
-    <label for="task-${div.index}" id="task-${div.index}" class="task-description">${div.description}</label><br>
+    <input type="checkbox" data-target="task-${div.index}" id="${div.index}" name="task-${div.index}" ${checked}>
+    <label style="text-decoration: ${state};" for="task-${div.index}" id="task-${div.index}" class="task-description">${div.description}</label><br>
     </div>
     <div><i class="fas fa-ellipsis-v"></i></div>
     </div>`;
   });
   TODOLIST_CONTAINER.innerHTML = task;
+  const checkboxes = document.querySelectorAll('input[type="checkbox"]');
+  checkboxes.forEach((checkbox) => {
+    const textDescription = document.getElementById(checkbox.dataset.target);
+    checkbox.addEventListener('change', (e) => {
+      interaction(e, todoListArray, textDescription);
+    });
+  });
 };
 
 const sortingFunction = (a, b) => {
@@ -44,43 +62,6 @@ const sortingFunction = (a, b) => {
   return comparison;
 };
 
-TODOLIST_ARRAY.sort(sortingFunction);
+todoListArray.sort(sortingFunction);
 
-// TODOLIST_ARRAY.forEach((object) => {
-// });
-localStorage.setItem('listOfTasks', JSON.stringify(TODOLIST_ARRAY));
-
-createToDoListDiv(TODOLIST_ARRAY);
-const checkboxes = document.querySelectorAll('input[type="checkbox"]');
-console.log(checkboxes);
-checkboxes.forEach((checkbox) => {
-  const textDescription = document.getElementById(checkbox.dataset.target);
-  console.log(checkbox);
-  checkbox.addEventListener('change', (e) => {
-    console.log('Hello!');
-    // TODOLIST_ARRAY.forEach((item) => {
-    //   if (item.index === checkbox.id) {
-    //     item.completed = e.target.checked;
-    //     // console.log(item);
-    //   }
-    // });
-    // localStorage.setItem('listOfTasks', JSON.stringify(TODOLIST_ARRAY));
-    if (e.target.checked) {
-      textDescription.style.textDecoration = 'line-through';
-    } else {
-      textDescription.style.textDecoration = 'none';
-    }
-    interaction(e);
-  });
-});
-
-function onPageLoad() {
-  if (JSON.parse(localStorage.getItem('listOfTasks'))) {
-    const listTasks = JSON.parse(localStorage.getItem('listOfTasks'));
-    createToDoListDiv(listTasks);
-  }
-}
-
-window.addEventListener('load', () => {
-  onPageLoad();
-});
+createToDoListDiv(todoListArray);
