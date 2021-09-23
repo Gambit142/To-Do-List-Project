128576115716cd013fa60b90ca8f997cb1703b78
Gambit142 <nzefrancismaryeugorji@gmail.com>
2021-09-23 20:47:37 +0100

    Create functions that allows users to edit task descriptions and store in local storage

--- src/functionality.js
+++ src/functionality.js
@@ -1,3 +1,4 @@
+// eslint-disable-next-line import/no-cycle
 export default (e, textField, todoListArray) => {
   if (textField.value === '') return;
   e.preventDefault();
@@ -7,7 +8,37 @@ export default (e, textField, todoListArray) => {
     index: todoListArray.length + 1,
   };
   todoListArray.push(localStorageObject);
-  console.log(todoListArray);
   localStorage.setItem('listOfTasks', JSON.stringify(todoListArray));
-//   createToDoListDiv(todoListArray);
+};
+
+export const editTasks = (array) => {
+  const individualTasks = document.querySelectorAll('.task-description');
+  individualTasks.forEach((tasks, taskIndex) => {
+    tasks.addEventListener('keypress', (e) => {
+      if (e.key === 'Enter') {
+        array.filter((object, objectIndex) => {
+          if (taskIndex === objectIndex) {
+            object.description = tasks.value;
+          }
+          return false;
+        });
+      }
+      localStorage.setItem('listOfTasks', JSON.stringify(array));
+    });
+  });
+};
+
+export const clickEditButton = () => {
+  const editHandler = document.querySelectorAll('.edit');
+  editHandler.forEach((edit) => {
+    const textDescription = document.getElementById(edit.dataset.target);
+    edit.addEventListener('click', () => {
+      textDescription.focus();
+      const moreButton = document.querySelectorAll('.more');
+      moreButton.forEach((btn) => {
+        const dropdownMenu = document.getElementById(btn.dataset.target);
+        dropdownMenu.classList.remove('show');
+      });
+    });
+  });
 };
