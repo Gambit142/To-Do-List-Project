7ab3aa4f151acc8bae60763fc2633f9afd45fb66
Gambit142 <nzefrancismaryeugorji@gmail.com>
2021-10-01 07:40:49 +0100

    Mock the CRUD functions by making them pure functions so that they can be tested easily

--- src/crud.js
+++ src/crud.js
@@ -1,6 +1,62 @@
-// eslint-disable-next-line import/prefer-default-export
-export const addData = (object) => {
-  const storageArray = [];
-  storageArray.push(object);
-  return storageArray;
+import { LocalStorage } from './localstorage.js';
+
+export const storage = new LocalStorage();
+
+export const addToDo = () => {
+  const localStorageObject = {
+    description: 'Task 1',
+    index: storage.array.length + 1,
+    completed: false,
+  };
+  storage.setLocalStorage(localStorageObject);
+};
+
+export const createHtmlDiv = (array) => {
+  let task = '';
+  array.forEach((element) => {
+    task += `<div class="task-div draggable" draggable="true" id="draggable-${element.index}">
+    <div>
+    <input type="checkbox" data-target="task-${element.index}" id="${element.index}" name="task-${element.index}">
+    <input type="text" value="${element.description}" id="task-${element.index}" class="task-description"><br>
+    </div>
+    <div><i class="fas fa-ellipsis-v more" data-target="button-${element.index}"></i></div>
+    </div>`;
+  });
+  const TODOLIST_CONTAINER = document.querySelector('.todo-lists-div');
+  TODOLIST_CONTAINER.innerHTML = task;
+};
+
+export const removeTodo = (index) => {
+  const filteredArray = storage.array.filter((task) => storage.array.indexOf(task) !== index);
+  filteredArray.forEach((obj, index) => {
+    obj.index = index + 1;
+  });
+  storage.array = filteredArray;
+};
+
+export const editTodo = (array, index) => {
+  const input = document.querySelector('.task-description');
+  array.filter((object, objectIndex) => {
+    if (index === objectIndex) {
+      object.description = input.value;
+    }
+    return false;
+  });
+};
+
+export const statusUpdate = (array, index) => {
+  array.filter((object, objectIndex) => {
+    if (index === objectIndex) {
+      object.completed = true;
+    }
+    return false;
+  });
+};
+
+export const deleteAllCompleted = (array) => {
+  const filteredArray = array.filter((obj) => obj.completed !== true);
+  filteredArray.forEach((obj, index) => {
+    obj.index = index + 1;
+  });
+  storage.array = filteredArray;
 };
